3fd34d5bf32937c5728e06500f18160ed95b4c32
govardhan-kl <klgovardhan87@gmail.com>
2025-04-29 23:28:57 +0530

    implemented filterItems, ResetDay and SetDailyLimit

--- js/app.js
+++ js/app.js
@@ -47,6 +47,19 @@ class CalorieTracker {
         }
     }
 
+    resetDay(){
+        this._totalCalories = 0;
+        this._meals = [];
+        this._workouts = [];
+        this._render();
+    }
+
+    setLimit(newLimitValue){
+        this._calorieLimit = newLimitValue;
+        this._displayCalorieLimit();
+        this._render();
+    }
+
     //private-methods
     _displayNewMeal(meal){
         const mealItems = document.getElementById('meal-items');
@@ -179,6 +192,13 @@ class App {
         document.getElementById('workout-form').addEventListener('submit',this._addNew_Item.bind(this, 'workout')); // we can still pass parameters in bind
         document.getElementById('meal-items').addEventListener('click', this._removeItem.bind(this,'meal'));
         document.getElementById('workout-items').addEventListener('click', this._removeItem.bind(this,'workout'));
+        //below one for filter items
+        document.getElementById('filter-meals').addEventListener("keyup",this._filterItems.bind(this,'meal'));
+        document.getElementById('filter-workouts').addEventListener('keyup',this._filterItems.bind(this,'workout'));
+        //below is for reset day
+        document.getElementById('reset').addEventListener('click', this._reset.bind(this));
+        //below is for set-daily limit
+        document.getElementById('limit-form').addEventListener('submit',this._setLimit.bind(this));
     }
 
     _addNew_Item(type,e){ //as we are passing arguments, arguments come first in the event object
@@ -222,6 +242,48 @@ class App {
             }
         }
     }
+
+    _filterItems(type,e){
+        const text = e.target.value.toLowerCase();
+        document.querySelectorAll(`#${type}-items .card`).forEach((item)=>{
+            const name = item.firstElementChild.firstElementChild.firstElementChild.textContent.trim();
+            if(name.toLowerCase().indexOf(text) !== -1 ){
+               item.style.display = 'block'; 
+            }else{
+                item.style.display = 'none';
+            }
+        })  
+    }
+
+    _reset(){
+        // this._tracker._calorieLimit = 4400;
+        console.log(this._tracker._calorieLimit);
+        this._tracker.resetDay();
+        document.getElementById('meal-items').innerHTML = '';
+        document.getElementById('workout-items').innerHTML = '';
+        document.getElementById('filter-meals').value = '';
+        document.getElementById('filter-workouts').value = '';
+    }
+
+    _setLimit(e){
+        e.preventDefault()
+        const limitUpdate = document.getElementById('limit')
+        if(limitUpdate.value === '' || Number(limitUpdate.value)<=0){
+            alert('please add a valid limit');
+            return;
+        }
+        if(Number(limitUpdate.value) !== this._tracker._calorieLimit){
+            this._tracker.setLimit(Number(limitUpdate.value));  
+        }
+        limitUpdate.value = '';
+        // document.getElementsByClassName('btn-close')[0].click()
+        //or close as below
+        const modalEl = document.getElementById('limit-modal');
+        const modal = bootstrap.Modal.getInstance(modalEl);
+        modal.hide();
+            
+    }
+        
 }
 
 const app = new App()
